35b305c403a515e13bf35523b60a6d4165e54c14
Jose Felix <joseenrfelixesp@gmail.com>
2023-04-30 17:27:23 -0700

    agregando metodo casoEspecialVerde

--- tarearuleta/JuegoDeLARuleta.java
+++ tarearuleta/JuegoDeLARuleta.java
@@ -11,6 +11,41 @@ public class JuegoDeLARuleta {
     static Scanner sc=new Scanner(System.in);
     public static float aleatorio;
 
+    public static void casoEspecialVerde(Jugador j, int i){
+        System.out.println("CASO ESPECIAL VERDE:");
+        String gano="nulo";
+        String usd1000="No";
+        String exito="";
+        int antes=j.getDinero();
+        int apuesta=j.getApuesta();
+        String result="verde";
+        while(result.equals("verde")){
+            result=monteCarlo();
+            if(!result.equals(j.getColorApostado()) && !result.equals("verde")){
+                int nuevoDinero=j.getDinero() - j.getApuesta();
+                j.setDinero(nuevoDinero);
+                gano="No";
+                if(j.getEstrategia().equals("doble")){
+                    int nuevaCantidadDeApuesta= 2 * j.getApuesta();
+                    j.setApuesta(nuevaCantidadDeApuesta);
+                    if(j.getApuesta() >= j.getDinero()){
+                        j.setApuesta(j.getDinero());
+                    }
+                    if(j.getApuesta() >= 1000){
+                        j.setApuesta(PRIMER_APUESTA);
+                    }
+                }
+            }
+            if(j.getDinero() >=1000){
+                usd1000="Si";
+                exito="++";
+            }
+            System.out.format("%17s %17s %17s %17s %17s %17s %17s %17s %17s", i, antes, 
+            apuesta, aleatorio, result, gano, j.getDinero(), usd1000, exito + "\n");
+        }
+        System.out.println("FIN CASO ESPECIAL VERDE");
+    }
+
     public static String monteCarlo(){
         aleatorio=(float) Math.random();
         if (aleatorio<0.454545){
@@ -31,6 +66,7 @@ public class JuegoDeLARuleta {
         int antes=j.getDinero();
         int apuesta=j.getApuesta();
         String result=monteCarlo();
+        boolean verde=false;
         if(j.getDinero()==0){
             return ;
         }
@@ -41,8 +77,7 @@ public class JuegoDeLARuleta {
         }
         else if(result.equals("verde")){
             gano="Nulo";
-            //TODO
-            //return;
+            verde=true;
         }
         else{
             int nuevoDinero=j.getDinero() - j.getApuesta();
@@ -65,16 +100,19 @@ public class JuegoDeLARuleta {
         }
         System.out.format("%17s %17s %17s %17s %17s %17s %17s %17s %17s", i, antes, 
         apuesta, aleatorio, result, gano, j.getDinero(), usd1000, exito + "\n");
+        if(verde){
+            casoEspecialVerde(j, i);
+        }
     }
 
     public static void main(String[] args) {
         System.out.format("%17s", "JUGADOR1\n");
         System.out.format("%17s %17s %17s %17s %17s %17s %17s %17s %17s", "iteracion", "$ antes", 
         "Apuesta", "#alea", "Color?", "Ganó?", "$ despues", "usd 1000?", "exito++\n");
-        for(int i=0; i<400; i++){
+        for(int i=0; i<1000; i++){
             juega(jugador1, i+1);
         }
-
+        System.out.println();
         System.out.format("%17s", "JUGADOR2\n");
         System.out.format("%17s %17s %17s %17s %17s %17s %17s %17s %17s", "iteracion", "$ antes", 
         "Apuesta", "#alea", "Color?", "Ganó?", "$ despues", "usd 1000?", "exito++\n");
